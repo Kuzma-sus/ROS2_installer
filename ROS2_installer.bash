519b88150f5d3d7f9d257f04df62c0aeaa73b061
Kuzma-sus <k66471373@gmail.com>
2025-12-14 15:04:30 +0300

    Add files via upload

---
+++ ROS2_installer.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# --- 1. Установка локалей ---
+sudo apt update && sudo apt install locales -y
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# --- 2. Добавление репозитория ROS 2 ---
+sudo apt update && sudo apt install curl gnupg2 lsb-release -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository universe -y
+sudo curl -sSL raw.githubusercontent.com -o /usr/share/keyrings/ros-archive-keyring.gpg
+# Исправлена обрезанная строка и добавлен tee
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# --- 3. Установка ROS 2 Jazzy ---
+sudo apt update -y
+sudo apt install ros-jazzy-desktop -y
+sudo apt install ros-dev-tools -y
+sudo apt install ros-jazzy-ros-base -y
+
+# --- 4. Настройка окружения ---
+# Исправлена команда добавления в .bashrc (убран '-y')
+echo "source /opt/ros/jazzy/setup.bash" >> ~/.bashrc
+# Исправлена команда применения настроек (убран '-y')
+source ~/.bashrc
+
+# --- 5. Установка инструментов сборки Python ---
+sudo apt install python3-pip -y
+
+# *** РЕШЕНИЕ ПРОБЛЕМЫ 'externally-managed-environment' ***
+# Используем флаг --break-system-packages для установки setuptools
+pip3 install setuptools --break-system-packages
+
+sudo apt install python3-colcon-common-extensions -y
+
+# --- 6. Проверка ---
+echo "Переменная ROS_DISTRO:"
+echo $ROS_DISTRO
+
